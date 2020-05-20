d97c42baba9870e45dca000c04ed0aad608030ca
Felipe Nicoletti Reis Mario <felipenicolettirmario@gmail.com>
2020-05-20 16:07:31 -0300

    Template inicial

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+export default function Index(){
+    return (
+        <>
+        <View>
+            <Text>
+                Hello World
+            </Text>
+        </View>
+        </>
+    )
+}
